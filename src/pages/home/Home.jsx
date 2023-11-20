ede75ef22a07a9f58285560e07593e9f34bc79e2
mohammedmzayek <mohammedmzayek9@gmail.com>
2023-11-20 01:16:45 -0800

    update project

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,14 @@
+import Filter from "../../components/filter/Filter";
+import HeroSection from "../../components/heroSection/HeroSection";
 import Layout from "../../components/layout/Layout";
 
 function Home() {
-  return <Layout>home</Layout>;
+  return (
+    <Layout>
+      <HeroSection />
+      <Filter />
+    </Layout>
+  );
 }
 
 export default Home;
