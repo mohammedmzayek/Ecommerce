f990b6bfdbdb9c096e92a744dbd27700c011e547
mohammedmzayek <mohammedmzayek9@gmail.com>
2024-01-03 13:57:20 -0800

    upload project

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,10 @@ import Cart from "./pages/cart/Cart.jsx";
 import Dashboard from "./pages/admin/dashboard/Dashboard.jsx";
 import NoPage from "./pages/nopage/nopage.jsx";
 import MyState from "./context/data/myState.jsx";
+import Login from "./pages/registration/Login";
+import Signup from "./pages/registration/Signup.jsx";
+import ProductInfo from "./pages/prodcutInfo/ProductInfo.jsx";
+
 function App() {
   return (
     <MyState>
@@ -13,8 +17,12 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/order" element={<Order />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/productinfo" element={<ProductInfo />} />
           <Route path="/cart" element={<Cart />} />
-          <Route path="/dahsboard" element={<Dashboard />} />
+
           <Route path="/*" element={<NoPage />} />
         </Routes>
       </Router>
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,18 @@
 import Filter from "../../components/filter/Filter";
 import HeroSection from "../../components/heroSection/HeroSection";
 import Layout from "../../components/layout/Layout";
+import ProductCard from "../../components/productCard/productCard";
+import Track from "../../components/track/Track";
+import Testimonial from "../../components/tetimonial/Testimonial";
 
 function Home() {
   return (
     <Layout>
       <HeroSection />
       <Filter />
+      <ProductCard />
+      <Track />
+      <Testimonial />
     </Layout>
   );
 }
